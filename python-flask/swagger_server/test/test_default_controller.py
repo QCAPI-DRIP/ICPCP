8532c8bccf50d7bb3a3cb4b141e40532afbe7fc3
skoulouzis <skoulouzis@users.noreply.github.com>
2020-04-23 13:42:33 +0000

    Generated code by SwaggerHub

---
+++ python-flask/swagger_server/test/test_default_controller.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.test import BaseTestCase
+
+
+class TestDefaultController(BaseTestCase):
+    """DefaultController integration test stubs"""
+
+    def test_upload_cwl(self):
+        """Test case for upload_cwl
+
+        upload a CWL workflow file
+        """
+        data = dict(file='file_example')
+        response = self.client.open(
+            '/workflow',
+            method='POST',
+            data=data,
+            content_type='multipart/form-data')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
