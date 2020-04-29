4edac06cacc13586fda038e5497c4fdefaafade8
skoulouzis <skoulouzis@users.noreply.github.com>
2020-04-29 12:00:23 +0000

    Generated code by SwaggerHub

--- python-flask/swagger_server/test/test_default_controller.py
+++ python-flask/swagger_server/test/test_default_controller.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import
 from flask import json
 from six import BytesIO
 
-from swagger_server.models.cwl_git import CWLGit  # noqa: E501
 from swagger_server.models.tosca_template import ToscaTemplate  # noqa: E501
 from swagger_server.test import BaseTestCase
 
@@ -13,28 +12,18 @@ from swagger_server.test import BaseTestCase
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
-    def test_add_workflow(self):
-        """Test case for add_workflow
-
-        add a cwl workflow
-        """
-        body = CWLGit()
-        response = self.client.open(
-            '/cwl',
-            method='POST',
-            data=json.dumps(body),
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
     def test_get_tosca_plan(self):
         """Test case for get_tosca_plan
 
         get workflow plan as tosca
         """
+        query_string = [('git_url', 'git_url_example'),
+                        ('branch', 'branch_example'),
+                        ('path', 'path_example')]
         response = self.client.open(
-            '/tosca/{cwl_id}'.format(cwl_id='cwl_id_example'),
-            method='GET')
+            '/tosca/',
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
