50069e7d7f7b57586e0a002361094647557052f9
Robin <r.m.p.linde@students.uu.nl>
2020-05-03 23:03:41 +0200

    Add getNumberOfTasks() method
    Count number of tasks in workflow

--- python-flask/service/q_cwop.py
+++ python-flask/service/q_cwop.py
@@ -3,9 +3,12 @@ from werkzeug.datastructures import FileStorage
 from toscaparser import shell as parser_shell
 from toscaparser.tosca_template import ToscaTemplate
 from legacy_code.ICPCP_TOSCA import Workflow
+import networkx as nx
+
 
 def save(file: FileStorage):
     dictionary = yaml.safe_load(file.stream)
+
 
 def writeToYaml(services):
     with open('data.yml', 'w') as outfile:
@@ -54,11 +57,31 @@ def runICPC():
         final_cost, final_eft = wf.cal_cost()
         print("final configuration: cost=" + str(final_cost) + "  EFT(exit)=" + str(final_eft))
 
+
 def verifyTOSCA():
-    #parser_shell.main("tosca_parser --template-file=python-flask/service/test1.yaml")
+    # parser_shell.main("tosca_parser --template-file=python-flask/service/test1.yaml")
     template = ToscaTemplate(path="C:\\Users\\robin\\Documents\\GitHub\\Q-CWOP\\python-flask\\service\\test.yaml")
     template.verify_template()
 
 
-if __name__ == '__main__':
+def getNumberOfTasks(filelocation):
+    with open(filelocation, 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+            if '$graph' in data:
+                graph = data['$graph']
+                for i in graph:
+                    if i['id'] == 'main':
+                        steps = i['steps']
+                        return len(steps)
+            else:
+                raise ValueError('Invalid workflow, $graph is missing')
 
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+if __name__ == '__main__':
+    #G = nx.DiGraph()
+    task_count = getNumberOfTasks('compile1.cwl')
+    print(task_count)
