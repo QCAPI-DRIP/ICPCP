f817ca998a6d8ffd654e6e8f35be9750f13be1fb
Robin <r.m.p.linde@students.uu.nl>
2020-05-11 17:45:44 +0200

    Improve readability

--- legacy_code/NewInstance.py
+++ legacy_code/NewInstance.py
@@ -1,5 +1,5 @@
 class NewInstance(object):
-    def __init__(self, vm_type, vm_start, vm_end, pcp):
+    def __init__(self, vm_type: int, vm_start: int, vm_end: int, pcp: list):
         self.vm_type = vm_type
         self.vm_start = vm_start
         self.vm_end = vm_end
