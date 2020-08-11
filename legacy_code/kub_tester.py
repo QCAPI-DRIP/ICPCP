c7d1aa16a83c3555bc514d8375b8a1ee1a93725a
Robin <r.m.p.linde@students.uu.nl>
2020-08-11 17:03:16 +0200

    Add kubernetes python client code

---
+++ legacy_code/kub_tester.py
@@ -0,0 +1,33 @@
+# from kubernetes.client import client, config
+import kubernetes.config as config
+import kubernetes.client as client
+from os import path
+import yaml
+
+config.load_kube_config()
+
+def list_pods():
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+def create_deployment():
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
+
+    with open(path.join(path.dirname(__file__), "deployment.yaml")) as f:
+        dep = yaml.safe_load(f)
+        k8s_apps_v1 = client.AppsV1Api()
+        resp = k8s_apps_v1.create_namespaced_deployment(
+            body=dep, namespace="default")
+        print("Deployment created. status='%s'" % resp.metadata.name)
+
+
+if __name__ == '__main__':
+    create_deployment()
+    list_pods()
