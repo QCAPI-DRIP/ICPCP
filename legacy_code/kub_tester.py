d9283e7a61bd63747cd90da6763f40abd315326a
Robin <r.m.p.linde@students.uu.nl>
2020-08-12 11:11:09 +0200

    Implement deployments and service for k8

--- legacy_code/kub_tester.py
+++ legacy_code/kub_tester.py
@@ -20,7 +20,14 @@ def create_deployment():
     # default location.
     config.load_kube_config()
 
-    with open(path.join(path.dirname(__file__), "deployment.yaml")) as f:
+    with open(path.join(path.dirname(__file__), "deployment_parser.yaml")) as f:
+        dep = yaml.safe_load(f)
+        k8s_apps_v1 = client.AppsV1Api()
+        resp = k8s_apps_v1.create_namespaced_deployment(
+            body=dep, namespace="default")
+        print("Deployment created. status='%s'" % resp.metadata.name)
+
+    with open(path.join(path.dirname(__file__), "deployment_planner.yaml")) as f:
         dep = yaml.safe_load(f)
         k8s_apps_v1 = client.AppsV1Api()
         resp = k8s_apps_v1.create_namespaced_deployment(
