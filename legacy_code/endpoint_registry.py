af6b6d45273ee8d91a90f7ae12a3aea2b3083843
Robin <r.m.p.linde@students.uu.nl>
2020-08-13 17:07:48 +0200

    Work on add_parser_endpoint()

--- legacy_code/endpoint_registry.py
+++ legacy_code/endpoint_registry.py
@@ -3,7 +3,9 @@ import external_apis.kub_create as kub_create
 class EndPointRegistry:
 
     def __init__(self):
+        """Parsers are defined as: {file_format : [endpoint]}"""
         self._parsers = {}
+        """Planners are defined as: {endpoint : [parameters]}"""
         self._planners = {}
 
     def add_parser_endpoint(self, name: str, image_repo: str, container_port: int, file_format: str):
@@ -11,10 +13,12 @@ class EndPointRegistry:
         Example endpoint_url: "http://localhost:5002/send_file"
         file_format: ".cwl"
         """
-        #add endpoint to kubernetes
-        kub_create.create_service(name, container_port)
-        kub_create.create_deployment(name, image_repo, container_port)
 
+        #TODO: Add check for available ports
+        #add endpoint to kubernetes
+        cluster_ip = kub_create.create_service(name, container_port)
+        kub_create.create_deployment(name, image_repo, container_port)
+        self._parsers[file_format] = [cluster_ip]
 
 
     def add_planner_endpoint(self, name: str, endpoint_url: str, planner_params: list):
