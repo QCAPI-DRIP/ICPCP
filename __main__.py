99fdcf6f9069d46d7933e34851e82051f9fbe19c
Robin <r.m.p.linde@students.uu.nl>
2020-08-19 12:29:14 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -140,7 +140,7 @@ def run_naive_planner(workflow_file_path, input_file_path):
 def request_metadata(workflow_file=None):
     """Request metadata from the parsers"""
     #request_url = "http://localhost:5002/send_file"
-    request_url = "cwl-parser-service/send_file"
+    request_url = "http://52.224.88.91:3002/send_file"
     if workflow_file is None:
         workflow_file = os.path.join(app.config['UPLOAD_FOLDER'], "compile1.cwl")
     files = {'file': open(workflow_file, 'rb')}
@@ -154,7 +154,7 @@ def request_metadata(workflow_file=None):
 
 def request_vm_sizes(parser_data):
     """"Request vm sizes from planner"""
-    request_url = "icpcp-planner-service/plan"
+    request_url = "http://52.224.90.144:3001/plan"
     #request_url = "icpcp-planner-service/plan"
     resp = requests.post(request_url, json=parser_data)
     plan_data = resp.json()
