b4d5d335f9e7396f1e96966aa7b1a7bfbe880cb4
Robin <r.m.p.linde@students.uu.nl>
2020-08-21 11:37:59 +0200

    Write absolute path definitions

---
+++ definitions.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+K8_CONFIG_PATH = os.path.join(ROOT_DIR, 'external_apis', 'config', 'config')
+ENDPOINTS_PATH = os.path.join(ROOT_DIR, 'endpoints')
