77f100f04060a66f4a6de7ae0b20723c3a9185e4
Robin <r.m.p.linde@students.uu.nl>
2020-08-25 13:18:20 +0200

    Add paths to definitions.py

--- definitions.py
+++ definitions.py
@@ -3,3 +3,5 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 K8_CONFIG_PATH = os.path.join(ROOT_DIR, 'external_apis', 'config', 'config')
 ENDPOINTS_PATH = os.path.join(ROOT_DIR, 'endpoints')
+PLANNING_INPUT = os.path.join(ROOT_DIR, 'planning_input')
+PLANNING_OUTPUT = os.path.join(ROOT_DIR, 'planning_output')
