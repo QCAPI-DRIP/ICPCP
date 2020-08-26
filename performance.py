c85260e24e57c4cdece0835d10f75b97858c7b4d
Robin <r.m.p.linde@students.uu.nl>
2020-08-26 12:38:37 +0200

    Create logs of performance metrics, do some validity checks

--- performance.py
+++ performance.py
@@ -25,26 +25,46 @@ def request_cluster():
 
 
 if __name__ == '__main__':
+    #clear log file
+    open('logs.txt', 'w').close()
+
+    #load input
     workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
     input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
-    # request_backend("localhost", 5001, workflow_file, input_file)
 
-    # number of requests to the power of 2 starting with 1 request, e.g. 1, 2, 4, 8, 16
-    number_of_requests = 2
+    # number of requests to the power of 2 starting with 1 request, e.g. setting it to 4 results in 1, 2, 4, 16
+    number_of_requests = 3
+    #change this if you want to raise to a different power
+    power = 2
+    # dont change this variable
     res = 2
-    for i in range(0, number_of_requests):
-        start = time.time()
-        if i == 0:
-            request_backend("localhost", 5001, workflow_file, input_file)
-        elif i == 1:
-            request_backend("localhost", 5001, workflow_file, input_file)
-            request_backend("localhost", 5001, workflow_file, input_file)
-        else:
-            res = res ** 2
-            for j in range(0, res):
-                request_backend("localhost", 5001, workflow_file, input_file)
-        response_time = time.time() - start
-        with open("logs.txt", "a") as text_file:
-            text_file.write("{} number of requests ---> {}s response time".format(res, response_time))
 
-        time.sleep(5)
+
+    with open("logs.txt", "a") as text_file:
+        for i in range(0, number_of_requests):
+            start = time.time()
+            if i == 0:
+                if not request_backend("localhost", 5001, workflow_file, input_file):
+                        text_file.write("After 1 request, no valid output ---> {}s response time \n".format(response_time))
+                        break
+
+
+            elif i == 1:
+                resp = request_backend("localhost", 5001, workflow_file, input_file)
+                resp2 = request_backend("localhost", 5001, workflow_file, input_file)
+                if not resp and resp2:
+                        text_file.write("After 2 requests, no valid output ---> {}s response time \n".format(response_time))
+                        break
+            else:
+                res = res ** power
+                for j in range(0, res):
+                    if not request_backend("localhost", 5001, workflow_file, input_file):
+                        break
+
+            response_time = time.time() - start
+            if i == 0:
+                text_file.write("1 number of requests ---> {}s response time \n".format(response_time))
+            else:
+                text_file.write("{} number of requests ---> {}s response time \n".format(res, response_time))
+
+            time.sleep(5)
