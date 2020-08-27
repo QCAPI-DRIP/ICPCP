06decbb85ddfe35b6c1698487c475fa53571922a
Robin <r.m.p.linde@students.uu.nl>
2020-08-27 10:35:59 +0200

    Update performance.py

--- performance.py
+++ performance.py
@@ -2,6 +2,8 @@ from definitions import PLANNING_INPUT
 import os
 import requests
 import time
+import aiohttp
+from concurrent.futures import ThreadPoolExecutor
 
 
 def request_backend(endpoint, port, workflow_file, input_file):
@@ -24,6 +26,23 @@ def request_cluster():
     resp = requests.post(request_url, files=files)
 
 
+def concurrent_requests():
+    # load input
+    workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
+    input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
+
+    # set ip and port of backend
+    backend_ip = "52.224.197.60"
+    backend_port = "3001"
+
+    pool = ThreadPoolExecutor(max_workers=5)
+
+    urls = ['http://www.google.com', 'http://www.yahoo.com', 'http://www.bing.com']  # Create a list of urls
+
+    for page in pool.map(fetch, urls):
+        # Do whatever you want with the results ...
+        print(page[0:100])
+
 if __name__ == '__main__':
     #clear log file
     open('logs.txt', 'w').close()
@@ -32,8 +51,12 @@ if __name__ == '__main__':
     workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
     input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
 
+    #set ip and port of backend
+    backend_ip = "52.224.197.60"
+    backend_port = "3001"
+
     # number of requests to the power of 2 starting with 1 request, e.g. setting it to 4 results in 1, 2, 4, 16
-    number_of_requests = 3
+    number_of_requests = 5
     #change this if you want to raise to a different power
     power = 2
     # dont change this variable
@@ -44,21 +67,21 @@ if __name__ == '__main__':
         for i in range(0, number_of_requests):
             start = time.time()
             if i == 0:
-                if not request_backend("localhost", 5001, workflow_file, input_file):
+                if not request_backend(backend_ip, backend_port, workflow_file, input_file):
                         text_file.write("After 1 request, no valid output ---> {}s response time \n".format(response_time))
                         break
 
 
             elif i == 1:
-                resp = request_backend("localhost", 5001, workflow_file, input_file)
-                resp2 = request_backend("localhost", 5001, workflow_file, input_file)
+                resp = request_backend(backend_ip, backend_port, workflow_file, input_file)
+                resp2 = request_backend(backend_ip, backend_port, workflow_file, input_file)
                 if not resp and resp2:
                         text_file.write("After 2 requests, no valid output ---> {}s response time \n".format(response_time))
                         break
             else:
                 res = res ** power
                 for j in range(0, res):
-                    if not request_backend("localhost", 5001, workflow_file, input_file):
+                    if not request_backend(backend_ip, backend_port, workflow_file, input_file):
                         break
 
             response_time = time.time() - start
