9feecf399829edc09153ba7794f8c45cc36f856a
Robin <r.m.p.linde@students.uu.nl>
2020-08-27 16:38:29 +0200

    Add support for concurrent requests

--- performance.py
+++ performance.py
@@ -4,6 +4,7 @@ import requests
 import time
 import aiohttp
 from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
 
 
 def request_backend(endpoint, port, workflow_file, input_file):
@@ -18,6 +19,7 @@ def request_backend(endpoint, port, workflow_file, input_file):
 
     return True
 
+
 def request_cluster():
     request_url = "http://52.188.135.248:80/send_file"
     workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
@@ -26,7 +28,7 @@ def request_cluster():
     resp = requests.post(request_url, files=files)
 
 
-def concurrent_requests():
+def concurrent_requests(number_of_requests_rounds, factor, interval):
     # load input
     workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
     input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
@@ -35,59 +37,99 @@ def concurrent_requests():
     backend_ip = "52.224.197.60"
     backend_port = "3001"
 
-    pool = ThreadPoolExecutor(max_workers=5)
+    # pool = ThreadPoolExecutor(max_workers=5)
+    #
+    # urls = ['http://www.google.com', 'http://www.yahoo.com', 'http://www.bing.com']  # Create a list of urls
+    #
+    # for page in pool.map(fetch, urls):
+    #     # Do whatever you want with the results ...
+    #     print(page[0:100])
 
-    urls = ['http://www.google.com', 'http://www.yahoo.com', 'http://www.bing.com']  # Create a list of urls
+    # We can use a with statement to ensure threads are cleaned up promptly
+    futures = []
+    number_of_requests = 1
+    for i in range(0, number_of_requests_rounds):
+        with ThreadPoolExecutor() as executor:
+            with open("logs.txt", "a") as text_file:
+                start_time = time.time()
+                for i in range(0, number_of_requests):
+                    futures.append(executor.submit(request_backend, backend_ip, backend_port, workflow_file, input_file))
 
-    for page in pool.map(fetch, urls):
-        # Do whatever you want with the results ...
-        print(page[0:100])
+                for future in as_completed(futures):
+                    try:
+                        if not future.result():
+                            text_file.write(
+                                "After {] request, no valid output \n".format(number_of_requests))
+                    except Exception as exc:
+                        print(exc)
+
+                response_time = time.time() - start_time
+                text_file.write(
+                    "{} number of requests ---> {}s response time \n".format(number_of_requests, response_time))
+
+                number_of_requests *= factor
+                time.sleep(interval)
+
+
+
 
 if __name__ == '__main__':
-    #clear log file
+    concurrent_reqs = True
+    #Starting with 1 request, each group of requests gets increased by specified factor
+    number_of_requests_rounds = 6
+    factor = 2
+    interval = 2
+
+    # clear log file
     open('logs.txt', 'w').close()
 
-    #load input
-    workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
-    input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
 
-    #set ip and port of backend
-    backend_ip = "52.224.197.60"
-    backend_port = "3001"
+    if concurrent_reqs:
+        concurrent_requests(number_of_requests_rounds, factor, interval)
+    else:
 
-    # number of requests to the power of 2 starting with 1 request, e.g. setting it to 4 results in 1, 2, 4, 16
-    number_of_requests = 5
-    #change this if you want to raise to a different power
-    power = 2
-    # dont change this variable
-    res = 2
+        # load input
+        workflow_file = os.path.join(PLANNING_INPUT, "compile1.cwl")
+        input_file = os.path.join(PLANNING_INPUT, "input_pcp.yaml")
 
+        # set ip and port of backend
+        backend_ip = "52.224.197.60"
+        backend_port = "3001"
 
-    with open("logs.txt", "a") as text_file:
-        for i in range(0, number_of_requests):
-            start = time.time()
-            if i == 0:
-                if not request_backend(backend_ip, backend_port, workflow_file, input_file):
-                        text_file.write("After 1 request, no valid output ---> {}s response time \n".format(response_time))
-                        break
+        # number of requests to the power of 2 starting with 1 request, e.g. setting it to 4 results in 1, 2, 4, 16
+        number_of_requests = 5
+        # change this if you want to raise to a different power
+        power = 2
+        # dont change this variable
+        res = 2
 
-
-            elif i == 1:
-                resp = request_backend(backend_ip, backend_port, workflow_file, input_file)
-                resp2 = request_backend(backend_ip, backend_port, workflow_file, input_file)
-                if not resp and resp2:
-                        text_file.write("After 2 requests, no valid output ---> {}s response time \n".format(response_time))
-                        break
-            else:
-                res = res ** power
-                for j in range(0, res):
+        with open("logs.txt", "a") as text_file:
+            for i in range(0, number_of_requests):
+                start = time.time()
+                if i == 0:
                     if not request_backend(backend_ip, backend_port, workflow_file, input_file):
+                        text_file.write(
+                            "After 1 request, no valid output ---> {}s response time \n".format(response_time))
                         break
 
-            response_time = time.time() - start
-            if i == 0:
-                text_file.write("1 number of requests ---> {}s response time \n".format(response_time))
-            else:
-                text_file.write("{} number of requests ---> {}s response time \n".format(res, response_time))
 
-            time.sleep(5)
+                elif i == 1:
+                    resp = request_backend(backend_ip, backend_port, workflow_file, input_file)
+                    resp2 = request_backend(backend_ip, backend_port, workflow_file, input_file)
+                    if not resp and resp2:
+                        text_file.write(
+                            "After 2 requests, no valid output ---> {}s response time \n".format(response_time))
+                        break
+                else:
+                    res = res ** power
+                    for j in range(0, res):
+                        if not request_backend(backend_ip, backend_port, workflow_file, input_file):
+                            break
+
+                response_time = time.time() - start
+                if i == 0:
+                    text_file.write("1 number of requests ---> {}s response time \n".format(response_time))
+                else:
+                    text_file.write("{} number of requests ---> {}s response time \n".format(res, response_time))
+
+                time.sleep(5)
