ef5af3901d112de03bb493927ec48f6f08152649
Robin <r.m.p.linde@students.uu.nl>
2020-09-04 17:38:58 +0200

    Add deviation

--- performance.py
+++ performance.py
@@ -58,13 +58,15 @@ def plot_multi_graph(x1, y1, x2, y2):
     # naming the y axis
     plt.ylabel('response time (s)')
     # giving a title to my graph
-    plt.title('Microservices vs monolithic')
+    plt.title('Microservices vs monolithic, avg=10')
 
     # show a legend on the plot
     plt.legend()
+    #
+    # # function to show the plot
+    # plt.show()
 
-    # function to show the plot
-    plt.show()
+    plt.savefig(os.path.join(EXPERIMENT_LOGS, "2replicas1node"))
 
 
 session_results_mono = {}
@@ -86,67 +88,63 @@ def concurrent_requests(number_of_requests_rounds, factor, interval, backend_ip,
     futures = []
     number_of_requests = 1
 
-    log_file_loc = os.path.join(EXPERIMENT_LOGS, "{}.txt".format(architecture_type))
-    with open(log_file_loc, "a") as text_file:
-        with open(os.path.join(EXPERIMENT_LOGS, '{}.csv'.format(architecture_type)), 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(["Number of requests", "Response time (s)"])
+    with open(os.path.join(EXPERIMENT_LOGS, '{}2replicas1node.csv'.format(architecture_type)), 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Number of requests", "Response time (s)"])
 
-            for i in range(0, number_of_requests_rounds):
-                # count = 0
-                start_time = time.time()
-                with ThreadPoolExecutor() as executor:
-                    for i in range(0, number_of_requests):
-                            futures.append(
-                                executor.submit(request_backend, backend_ip, backend_port, workflow_file, input_file))
+        for i in range(0, number_of_requests_rounds):
+            # count = 0
+            start_time = time.time()
+            with ThreadPoolExecutor() as executor:
+                for i in range(0, number_of_requests):
+                        futures.append(
+                            executor.submit(request_backend, backend_ip, backend_port, workflow_file, input_file))
 
-                    for future in as_completed(futures):
-                        try:
-                            result = future.result()
-                            if not result:
-                                print("After {} requests, no valid output \n".format(number_of_requests))
-                                text_file.write("After {} requests, no valid output \n".format(number_of_requests))
-                                return False
-                            # if result:
-                            #     count +=1
-
-                        except Exception as exc:
-                            print(exc)
+                for future in as_completed(futures):
+                    try:
+                        result = future.result()
+                        if not result:
+                            writer.writerow([number_of_requests, "Invalid output"])
+                            print("After {} requests, no valid output \n".format(number_of_requests))
                             return False
+                        # if result:
+                        #     count +=1
+
+                    except Exception as exc:
+                        print(exc)
+                        return False
 
 
-                    futures.clear()
-                    # print(count)
-                    response_time = time.time() - start_time
+                futures.clear()
+                # print(count)
+                response_time = time.time() - start_time
 
 
-                #log results
-                writer.writerow([number_of_requests, response_time])
-                text_file.write(
-                    "{} number of requests ---> {}s response time \n".format(number_of_requests, response_time))
+            #log results
+            writer.writerow([number_of_requests, response_time])
 
-                if architecture_type == "mono":
-                    if number_of_requests in session_results_mono:
-                        dict_value = session_results_mono[number_of_requests]
-                        dict_value.append(response_time)
-                        session_results_mono[number_of_requests] = dict_value
-
-                    else:
-                        session_results_mono[number_of_requests] = [response_time]
+            if architecture_type == "mono":
+                if number_of_requests in session_results_mono:
+                    dict_value = session_results_mono[number_of_requests]
+                    dict_value.append(response_time)
+                    session_results_mono[number_of_requests] = dict_value
 
                 else:
-                    if number_of_requests in session_results_micro:
-                        dict_value = session_results_micro[number_of_requests]
-                        dict_value.append(response_time)
-                        session_results_micro[number_of_requests] = dict_value
+                    session_results_mono[number_of_requests] = [response_time]
 
-                    else:
-                        session_results_micro[number_of_requests] = [response_time]
+            else:
+                if number_of_requests in session_results_micro:
+                    dict_value = session_results_micro[number_of_requests]
+                    dict_value.append(response_time)
+                    session_results_micro[number_of_requests] = dict_value
 
-                x_axis_requests.append(number_of_requests)
-                y_axis_response_time.append(response_time)
-                number_of_requests *= factor
-                time.sleep(interval)
+                else:
+                    session_results_micro[number_of_requests] = [response_time]
+
+            x_axis_requests.append(number_of_requests)
+            y_axis_response_time.append(response_time)
+            number_of_requests *= factor
+            time.sleep(interval)
 
     return x_axis_requests, y_axis_response_time
 
@@ -174,7 +172,7 @@ if __name__ == '__main__':
     concurrent_reqs = True
     print_graph = False
     # Starting with 1 request, each group of requests gets increased by specified factor
-    number_of_requests_rounds = 3
+    number_of_requests_rounds = 2
     factor = 2
     interval = 2
 
@@ -191,9 +189,9 @@ if __name__ == '__main__':
     log_file_loc_mono = os.path.join(EXPERIMENT_LOGS, "{}.txt".format("mono"))
     log_file_loc_micro = os.path.join(EXPERIMENT_LOGS, "{}.txt".format("micro"))
 
-    # clear log files
-    open(log_file_loc_mono, 'w').close()
-    open(log_file_loc_micro, 'w').close()
+    # # clear log files
+    # open(log_file_loc_mono, 'w').close()
+    # open(log_file_loc_micro, 'w').close()
 
     if concurrent_reqs:
         for _ in range(0, number_of_experiment_rounds):
@@ -211,17 +209,35 @@ if __name__ == '__main__':
         #do computations
         number_of_requests_x_axis_mono = []
         avg_performance_time_y_axis_mono = []
+        deviation_mono = []
 
         number_of_requests_x_axis_micro = []
         avg_performance_time_y_axis_micro = []
+        deviation_micro = []
 
-        for key, value in session_results_mono.items():
-            number_of_requests_x_axis_mono.append(key)
-            avg_performance_time_y_axis_mono.append(numpy.mean(value))
+        with open(os.path.join(EXPERIMENT_LOGS, 'mono_avg_2replicas1node.csv'), 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Number of requests", "Average response time (s)", "Standard deviation"])
 
-        for key, value in session_results_micro.items():
-            number_of_requests_x_axis_micro.append(key)
-            avg_performance_time_y_axis_micro.append(numpy.mean(value))
+            for key, value in session_results_mono.items():
+                number_of_requests_x_axis_mono.append(key)
+                mean = numpy.mean(value)
+                deviation = numpy.std(value)
+                avg_performance_time_y_axis_mono.append(mean)
+                deviation_mono.append(deviation)
+                writer.writerow([key, mean, deviation])
+
+        with open(os.path.join(EXPERIMENT_LOGS, 'micro_avg_2replicas1node.csv'), 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Number of requests", "Average response time (s), Standard deviation"])
+
+            for key, value in session_results_micro.items():
+                number_of_requests_x_axis_micro.append(key)
+                mean = numpy.mean(value)
+                deviation = numpy.std(value)
+                avg_performance_time_y_axis_micro.append(mean)
+                deviation_micro.append(deviation)
+                writer.writerow([key, mean, deviation])
 
         plot_multi_graph(number_of_requests_x_axis_mono, avg_performance_time_y_axis_mono, number_of_requests_x_axis_micro, avg_performance_time_y_axis_micro)
 
